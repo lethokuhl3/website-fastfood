afec44e8be6dd05bbc36f1f0a4fbb2bdde04181d
lethokuhleDev <you@example.com>
2026-07-23 19:50:49 +0200

    Add route to serve HTML file for all unmatched requests

--- server.js
+++ server.js
@@ -10,7 +10,10 @@ const PORT = process.env.PORT || 5000;
 // Middleware
 app.use(cors());
 app.use(express.json());
-app.use(express.static(path.join(__dirname, "frontend"))); // Serves your HTML, CSS, JS files automatically
+app.use(express.static(path.join(__dirname, "frontend")));
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "frontend", "h.html"));
+});
 
 // Connect to MongoDB
 mongoose
