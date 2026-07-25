69942b06f8fd3f05dc73a861142aaa74bf8d43e2
lethokuhleDev <you@example.com>
2026-07-25 12:49:42 +0200

    Add express.urlencoded middleware for parsing URL-encoded data

--- server.js
+++ server.js
@@ -10,6 +10,7 @@ const PORT = process.env.PORT || 5000;
 // Middleware
 app.use(cors());
 app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, "frontend")));
 app.get("*", (req, res) => {
   res.sendFile(path.join(__dirname, "frontend", "h.html"));
